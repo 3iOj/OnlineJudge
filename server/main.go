2ecd112e09f355b823318b0985905e2e5a8d7ea6
thewackyindian <thewackyindian@gmail.com>
2023-06-12 13:52:37 +0530

    updated connections

--- server/main.go
+++ server/main.go
@@ -20,10 +20,12 @@ func main() {
 	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
-	}
+	}	
 	store := db.NewStore(conn)
-	server := api.NewServer(store)
-
+	server, err := api.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
 	server.Start(config.ServerAddress)
 
 	if err != nil {
