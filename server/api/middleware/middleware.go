6cf1cb49cbbccaf24ae8fca004b017ca798ed86f
thewackyindian <thewackyindian@gmail.com>
2023-07-09 18:27:01 +0530

    added cors middleware

--- server/api/middleware/middleware.go
+++ server/api/middleware/middleware.go
@@ -16,7 +16,21 @@ const (
 	AuthorizationPayloadKey = "authorization_payload"
 )
 
+func CorsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
 
 func AuthMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
